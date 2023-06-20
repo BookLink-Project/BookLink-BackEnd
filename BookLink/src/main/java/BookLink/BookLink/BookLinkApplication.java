c5804f95482aa590a6f23b87412b9fe5c18adb40
yuseok0215 <elwlahstmxjf@naver.com>
2023-06-21 00:45:51 +0900

    기본 디렉토리 생성

---
+++ BookLink/src/main/java/BookLink/BookLink/BookLinkApplication.java
@@ -0,0 +1,13 @@
+package BookLink.BookLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLinkApplication.class, args);
+	}
+
+}
---
+++ BookLink/src/test/java/BookLink/BookLink/BookLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package BookLink.BookLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
