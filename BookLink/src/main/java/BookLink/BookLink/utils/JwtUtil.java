8463281359d38b9e47425f3cbd4a935b1f6cec1e
SCY <helenason@naver.com>
2023-06-26 20:50:30 +0900

    feat: Spring Security 설정 및 JWT 구현

---
+++ BookLink/src/main/java/BookLink/BookLink/utils/JwtUtil.java
@@ -0,0 +1,33 @@
+package BookLink.BookLink.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    public static String getEmail(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().get("email", String.class);
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
+                .getBody().getExpiration().before(new Date()); // 지금보다 이전인지
+    }
+
+    public static String createJwt(String email, String secretKey, Long expiredMs) {
+
+        Claims claims = Jwts.claims(); // claim : JWT가 제공하는 공간 (일종의 map)
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+}
