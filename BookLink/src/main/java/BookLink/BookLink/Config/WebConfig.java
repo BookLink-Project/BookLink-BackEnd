fa598769ae74fda47284b500360b2cf57e9570e1
yuseok0215 <elwlahstmxjf@naver.com>
2023-07-04 18:21:22 +0900

    feat: cors 에러 해결 코드

---
+++ BookLink/src/main/java/BookLink/BookLink/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package BookLink.BookLink.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //CORS : Cross Origin을 열어주는것을 매 컨트롤러마다 설정 x. 아래와같이설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
