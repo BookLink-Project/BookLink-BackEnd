5f6f36ba7f4f19e98521b5719569345096b25b4e
yuseok0215 <elwlahstmxjf@naver.com>
2023-07-04 23:27:31 +0900

    refactor: allowedOrigins에 경로 추가

--- BookLink/src/main/java/BookLink/BookLink/Config/WebConfig.java
+++ BookLink/src/main/java/BookLink/BookLink/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
