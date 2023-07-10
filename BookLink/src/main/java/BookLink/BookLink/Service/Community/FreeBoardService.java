5583a11127328815e3cf3a93267a453058af6aec
SCY <helenason@naver.com>
2023-07-10 20:02:46 +0900

    feat: 커뮤니티 자유글 작성

---
+++ BookLink/src/main/java/BookLink/BookLink/Service/Community/FreeBoardService.java
@@ -0,0 +1,10 @@
+package BookLink.BookLink.Service.Community;
+
+import BookLink.BookLink.Domain.Community.FreeBoardDto;
+import BookLink.BookLink.Domain.ResponseDto;
+
+public interface FreeBoardService {
+
+    ResponseDto writePost(String memEmail, FreeBoardDto freeBoardDto);
+
+}
