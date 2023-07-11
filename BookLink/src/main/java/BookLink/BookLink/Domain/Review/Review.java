f26d749f502ec77f568fbd4bff8c1f20ea233367
SCY <helenason@naver.com>
2023-07-11 21:38:27 +0900

    feat: 책방 후기 대댓글 도메인 추가

--- BookLink/src/main/java/BookLink/BookLink/Domain/Review/Review.java
+++ BookLink/src/main/java/BookLink/BookLink/Domain/Review/Review.java
@@ -1,9 +1,14 @@
 package BookLink.BookLink.Domain.Review;
 
+import BookLink.BookLink.Domain.Common.BaseTimeEntity;
 import BookLink.BookLink.Domain.Member.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -12,7 +17,9 @@ import java.util.Date;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Review {
+@DynamicInsert // for default 적용
+@DynamicUpdate // 변경 필드만 update 반영
+public class Review extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,18 +38,37 @@ public class Review {
     @JoinColumn(name = "writer")
     private Member writer;
 
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date date;
-
     @NotNull
     private String content;
 
-    // 좋아요, 싫어요, 답글 추가 예정
+//    @NotNull
+    @ColumnDefault("0")
+    private Long like_cnt;
+
+//    @NotNull
+    @ColumnDefault("0")
+    private Long hates_cnt;
+
+//    @NotNull // test
+    @ManyToOne
+    @JoinColumn(name = "parent")
+    private Review parent; // 가짜 매핑 X
+
+//    @NotNull // test
+    private boolean isDeleted;
+
+//    @NotNull
+//    private boolean isModified;
+
+    public void updateParent(Review parent) {
+        this.parent = parent;
+    }
 
     @Builder
-    public Review(String isbn, Member writer, String content) {
+    public Review(String isbn, Member writer, String content, Review parent) {
         this.isbn = isbn;
         this.writer = writer;
         this.content = content;
+        this.parent = parent;
     }
 }
