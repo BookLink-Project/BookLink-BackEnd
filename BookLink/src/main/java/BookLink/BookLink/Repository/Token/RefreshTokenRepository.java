fde5ad3e279e64e971b4ac753e7ef2bf7f772fc3
SCY <helenason@naver.com>
2023-07-13 02:31:02 +0900

    feat: JWT 토큰 만료 시 response 전달

--- BookLink/src/main/java/BookLink/BookLink/Repository/Token/RefreshTokenRepository.java
+++ BookLink/src/main/java/BookLink/BookLink/Repository/Token/RefreshTokenRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByMemberEmail(String email);
+
+    Optional<RefreshToken> findByToken(String token);
 }
