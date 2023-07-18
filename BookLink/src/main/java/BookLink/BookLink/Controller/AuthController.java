56b64ca589274b76fbe26553ccc2b832cf433159
yuseok0215 <elwlahstmxjf@naver.com>
2023-07-18 23:41:11 +0900

    feat: OAuth2.0 카카오 소셜 로그인 token 받아오기 구현

--- BookLink/src/main/java/BookLink/BookLink/Controller/AuthController.java
+++ BookLink/src/main/java/BookLink/BookLink/Controller/AuthController.java
@@ -1,27 +1,42 @@
 package BookLink.BookLink.Controller;
 
+import BookLink.BookLink.OAuth.OAuthService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Authorization 을 위한 접근 테스트 controller 입니다.
  */
 
 @RestController
-@RequestMapping("/api/auth-test")
+@AllArgsConstructor
+//@RequestMapping("/api/auth-test")
+@RequestMapping("login/oauth2/code")
 public class AuthController {
+
+    private final OAuthService oAuthService;
 
     @PostMapping
     public ResponseEntity<String> authTest() { // with cookie input
 
         return ResponseEntity.ok().body("접근 성공");
 
+    }
+
+    /**
+     * 카카오 callback
+     * [GET] /oauth/kakao/callback
+     */
+    @GetMapping("/kakao")
+    public void kakaoCallback(@RequestParam String code) {
+        System.out.println(code);
+        String kakaoAccessToken = oAuthService.getKakaoAccessToken(code);
+        oAuthService.createKakaoUser(kakaoAccessToken);
     }
 
 }
