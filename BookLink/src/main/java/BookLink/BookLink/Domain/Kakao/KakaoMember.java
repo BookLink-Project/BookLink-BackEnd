bebf599fb58132d17d80b4b6919ded7260b7686a
yuseok0215 <elwlahstmxjf@naver.com>
2023-07-27 16:02:53 +0900

    refactor: OAuthService 멤버 저장 코드 수정

---
+++ BookLink/src/main/java/BookLink/BookLink/Domain/Kakao/KakaoMember.java
@@ -0,0 +1,10 @@
+package BookLink.BookLink.Domain.Kakao;
+
+public class KakaoMember {
+
+    private String nickname;
+    private String profile_image_url;
+
+    private String account_email;
+    private String birthday;
+}
---
+++ BookLink/src/main/java/BookLink/BookLink/Domain/Member/KakaoMemberDto.java
@@ -0,0 +1,44 @@
+package BookLink.BookLink.Domain.Member;
+
+import BookLink.BookLink.Domain.Common.SocialType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class KakaoMemberDto {
+
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request{
+        private String nickname;
+        private String thumbnail_image_url;
+        private String profile_image_url;
+        private String is_default_image;
+
+        public static Member toEntity(KakaoMemberDto.Request request, String email) throws MalformedURLException {
+            return Member.builder()
+                    .email(email)
+                    .nickname(request.getNickname())
+                    .image(toUrl(request.profile_image_url))
+                    .socialType(SocialType.KAKAO)
+                    .build();
+        }
+
+
+    }
+    public static URL toUrl(String profile_image_url) throws MalformedURLException {
+        URL url = new URL(profile_image_url);
+        return url;
+    }
+
+
+}
