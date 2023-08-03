1f345ea4a7f25cc54b7475bdebc9e4e4ac4d0dea
yuseok0215 <elwlahstmxjf@naver.com>
2023-08-03 14:16:48 +0900

    refactor: 프론트 서버 CORS 에러 해결

--- BookLink/src/main/java/BookLink/BookLink/Config/WebConfig.java
+++ BookLink/src/main/java/BookLink/BookLink/Config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173")
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:5173",
+                        "http://127.0.0.1:5173","http://52.79.70.115")
                 .allowCredentials(true)
                 .allowedMethods(
                         HttpMethod.GET.name(),
