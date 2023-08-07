11ee6b2da79903086ce41c1aea435086e2e06ef9
SCY <helenason@naver.com>
2023-08-08 03:11:02 +0900

    feat: 서버 타임존 변경

--- BookLink/src/main/java/BookLink/BookLink/BookLinkApplication.java
+++ BookLink/src/main/java/BookLink/BookLink/BookLinkApplication.java
@@ -1,12 +1,24 @@
 package BookLink.BookLink;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Slf4j
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class BookLinkApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("현재 시각 : " + new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookLinkApplication.class, args);
