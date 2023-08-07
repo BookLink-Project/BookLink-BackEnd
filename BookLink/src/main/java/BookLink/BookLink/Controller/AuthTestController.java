654e2e77bf9872fb9ee44cb63a36ad037fdf2b50
yuseok0215 <elwlahstmxjf@naver.com>
2023-08-07 22:47:36 +0900

    refactor: merge에서 제외된 부분 추가

--- BookLink/src/main/java/BookLink/BookLink/Controller/AuthTestController.java
+++ BookLink/src/main/java/BookLink/BookLink/Controller/AuthTestController.java
@@ -1,8 +1,10 @@
 package BookLink.BookLink.Controller;
 
-
+import BookLink.BookLink.Domain.Member.MemberPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,7 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login/oauth2")
+@RequestMapping("/api/v1/auth-test")
 public class AuthTestController {
 
+    @PostMapping
+    public ResponseEntity<String> authTest(@AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+
+        return ResponseEntity.ok().body("접근 성공");
+
+    }
 }
