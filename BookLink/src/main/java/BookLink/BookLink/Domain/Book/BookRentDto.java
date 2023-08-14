5971363bd5a0a268b7d37f420516f944f334af58
yuseok0215 <elwlahstmxjf@naver.com>
2023-08-14 15:55:40 +0900

    refactor: BookRentDto 클래스에 pub_date 추가

--- BookLink/src/main/java/BookLink/BookLink/Domain/Book/BookRentDto.java
+++ BookLink/src/main/java/BookLink/BookLink/Domain/Book/BookRentDto.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
 @NoArgsConstructor
 public class BookRentDto {
@@ -11,16 +13,19 @@ public class BookRentDto {
     private String title;
     private String authors;
     private String isbn;
+    private LocalDate pub_date;
     private String cover;
     private String publisher;
     private Integer avg_rental_fee;
     private Integer rent_period;
 
     @Builder
-    public BookRentDto(String title, String authors, String isbn, String cover, String publisher, Integer avg_rental_fee, Integer rent_period) {
+    public BookRentDto(String title, String authors, String isbn, LocalDate pub_date, String cover, String publisher,
+                       Integer avg_rental_fee, Integer rent_period) {
         this.title = title;
         this.authors = authors;
         this.isbn = isbn;
+        this.pub_date = pub_date;
         this.cover = cover;
         this.publisher = publisher;
         this.avg_rental_fee = avg_rental_fee;
