28b388c49c54612be97b3642c380683a12c89ff8
SCY <helenason@naver.com>
2023-08-18 03:10:58 +0900

    feat: 책방 내 주변 대여 정보

--- BookLink/src/main/java/BookLink/BookLink/Repository/Book/BookRentRepository.java
+++ BookLink/src/main/java/BookLink/BookLink/Repository/Book/BookRentRepository.java
@@ -2,10 +2,15 @@ package BookLink.BookLink.Repository.Book;
 
 import BookLink.BookLink.Domain.Book.BookRent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface BookRentRepository extends JpaRepository<BookRent, Long> {
 
     void deleteById(Long id);
+    @Query("SELECT br FROM BookRent br WHERE br.rent_location LIKE :location%")
+    List<BookRent> findByRentLocation(@Param("location") String location);
+
 }
