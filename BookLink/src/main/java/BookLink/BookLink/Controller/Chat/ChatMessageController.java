458792d6965832b725a66d6d4ce0277add718f26
SCY <helenason@naver.com>
2023-08-19 23:17:08 +0900

    feat: 소켓 채팅 토큰 사용 코드 추가

--- BookLink/src/main/java/BookLink/BookLink/Controller/Chat/ChatMessageController.java
+++ BookLink/src/main/java/BookLink/BookLink/Controller/Chat/ChatMessageController.java
@@ -1,6 +1,8 @@
 package BookLink.BookLink.Controller.Chat;
 
 import BookLink.BookLink.Domain.Chat.ChatMessageDto;
+import BookLink.BookLink.Domain.Member.Member;
+import BookLink.BookLink.Domain.Member.MemberPrincipal;
 import BookLink.BookLink.Domain.ResponseDto;
 import BookLink.BookLink.Service.Chat.ChatService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,18 +23,20 @@ public class ChatMessageController {
     private final ChatService chatService;
 
     @MessageMapping("/api/v1/chat/message")
-    public ResponseEntity<ResponseDto> enter(@RequestBody ChatMessageDto.Request message) { // + @AuthenticationPrincipal
+    public ResponseEntity<ResponseDto> enter(@RequestBody ChatMessageDto.Request message,
+                                             @AuthenticationPrincipal MemberPrincipal memberPrincipal) { // check
+
 //        if (ChatMessageDto.MessageType.ENTER.equals(message.getType())) {
 //            message.setMessage(message.getSender()+"님이 입장하였습니다.");
 //        }
 
         log.info("===MessageMapping START===");
 
-//        Member sender = memberPrincipal.getMember();
-//        log.info("sender={}", sender.getNickname());
+        Member sender = memberPrincipal.getMember();
+        log.info("sender={}", sender.getNickname());
 //
-//        ResponseDto responseDto = chatService.sendMessage(message, sender);
-        ResponseDto responseDto = chatService.sendMessage(message);
+        ResponseDto responseDto = chatService.sendMessage(message, sender);
+//        ResponseDto responseDto = chatService.sendMessage(message);
 
         simpMessagingTemplate.convertAndSend("/sub/api/v1/chat/room/"+ message.getRoom_id(), message);
 
